bb1aec6a66bc6974bb48668676b9a133757bb903
ss groups cybercell <sujeetsinghmuz22@gmail.com>
2025-12-27 12:54:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    data = request.json
+    name = data.get("name")
+    email = data.get("email")
+
+    # Terminal me show
+    print("Name:", name)
+    print("Email:", email)
+
+    # Data file me save
+    with open("data.txt", "a") as f:
+        f.write(f"{name}, {email}\n")
+
+    return jsonify({"message": "Data save ho gaya aur backend ko mil gaya"})
+    
+app.run(port=5000)
