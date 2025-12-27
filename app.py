7399aa5244d8b00a21c5304fc4cffc9f0d8c94b5
ss groups cybercell <sujeetsinghmuz22@gmail.com>
2025-12-27 14:51:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -19,5 +20,7 @@ def submit():
         f.write(f"{name}, {email}\n")
 
     return jsonify({"message": "congratulations"})
-    
-app.run(port=5000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
